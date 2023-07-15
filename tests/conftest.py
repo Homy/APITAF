440256cdaee07a44d1acb91b5f781e7db4e0494c
Homy <honta.id@gmail.com>
2023-07-15 11:24:07 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,29 @@
 import pytest
 import yaml
 from clients.api_client import APIClient
+from clients.users import Users
 from clients.posts import Posts
+
 
 def load_config():
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
     return config
 
+
 config = load_config()
 BASE_URL = config["base_url"]
+
 
 @pytest.fixture(scope="module")
 def api_client():
     return APIClient(BASE_URL)
 
+
 @pytest.fixture(scope="module")
 def users(api_client):
     return Users(api_client)
+
 
 @pytest.fixture(scope="module")
 def posts(api_client):
